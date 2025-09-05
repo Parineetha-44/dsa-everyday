d3657222eaa4def555e5766ce5e0378475951c1f
Parineetha-44 <parineetha247@gmail.com>
2025-09-05 21:17:29 +0530

    Time: 4 ms (8.38%) | Memory: 17.6 MB (91.97%) - LeetSync

---
+++ stack/1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        arr=[]
+        for i in range(1,n+1):
+            if n%i==0:
+                arr.append(i)
+        if k>len(arr):
+            return -1
+        elif k<=len(arr):
+            return arr[k-1]
+        
+
+
+        
