fb97c6e3a38e344ff9b8db00e38b8c7a407a9af9
Parineetha-44 <parineetha247@gmail.com>
2026-06-24 14:30:26 +0530

    Time: 4 ms (17.61%) | Memory: 19.2 MB (54.93%) - LeetSync

---
+++ stack/2736-minimum-additions-to-make-valid-string/minimum-additions-to-make-valid-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        groups = 1
+
+        for i in range(1, len(word)):
+            if word[i] <= word[i - 1]:
+                groups += 1
+
+        return groups * 3 - len(word)
